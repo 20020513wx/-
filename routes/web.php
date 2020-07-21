8ef7c7d8413c82e5ca626952785223e1d212239f
Your Name <you@example.com>
2020-07-21 20:43:12 +0800

    7.21.8:43mmm

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//前台
 Route::prefix('index')->group(function(){
 	Route::get('/goodsindex','Index\GoodsController@goodsindex');
 	Route::get('/goodslists','Index\GoodsController@goodslists');
@@ -20,3 +20,14 @@ Route::prefix('index')->group(function(){
 Route::prefix("/index")->group(function(){
     Route::get('index/cart','Index\CartController@cart');
 });
+
+
+//后台用户
+Route::prefix("/admin")->group(function(){
+    Route::get('/userindex','Admin\UserController@userindex');//用户展示
+    Route::get('delete/{id}','Admin\UserController@delete');//删除
+    Route::get('edit/{id}','Admin\UserController@edit');//编辑展示
+    Route::post('update/{id}','Admin\UserController@update');//编辑执行
+});
+
+

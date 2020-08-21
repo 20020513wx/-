c529823226de595bbf9551222a4cb54f436ead77
wangxiao <1513505862@qq.com>
2020-08-21 21:11:25 +0800

    1

--- app/Http/Middleware/CheckLogin.php
+++ app/Http/Middleware/CheckLogin.php
@@ -3,6 +3,9 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Redis;
 
 class CheckLogin
 {
@@ -15,10 +18,23 @@ class CheckLogin
      */
     public function handle($request, Closure $next)
     {
-        $id=session('id');
-        if(empty($id)){
-            return redirect('/index/login');
+        $_SERVER['uid'] = 0;        //默认未登录
+        $token = Cookie::get('token');
+
+        if($token)
+        {
+            //$token = Crypt::decryptString($token);        //解密cookie
+            $token_key = 'h:login_info:'.$token;
+            $u = Redis::hGetAll($token_key);
+
+            if(isset($u['uid']))        // 登录有效
+            {
+                $_SERVER['uid'] = $u['uid'];
+                $_SERVER['user_name'] = $u['user_name'];
+            }
         }
+
+
         return $next($request);
     }
 }
